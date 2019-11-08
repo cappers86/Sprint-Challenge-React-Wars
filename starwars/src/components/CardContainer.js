c4bd3754346ebbc9d83c110038ac3c03b0a991b1
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-11-08 08:53:53 +0000

    Did first call to API checked to see if i am recieveing data

--- starwars/src/components/CardContainer.js
+++ starwars/src/components/CardContainer.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import Card from './Card'
+import React, { useEffect } from 'react';
+import Card from './Card';
+import axios from 'axios';
+
 
 const CardContainer = () => {
 
+
+useEffect (() => {
+
+    axios.get('https://swapi.co/api/people/')
+          .then(responce => {
+              console.log(responce);
+          })
+          .catch(error => {
+              console.log(error);
+          })
+}, [])
 
   return (
     <div className="CardContainer">
