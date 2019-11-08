5b9e50bf4df44a699fb32b0015bb388e17ddbc42
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-11-08 09:28:55 +0000

    Card test displaying the ammount of objects

--- starwars/src/components/CardContainer.js
+++ starwars/src/components/CardContainer.js
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from './Card';
 import axios from 'axios';
 
 
 const CardContainer = () => {
+    const [people, fetchPeople] = useState([]);
 
 
 useEffect (() => {
 
     axios.get('https://swapi.co/api/people/')
           .then(responce => {
-              console.log(responce);
+              console.log(responce.data.results);
+              fetchPeople(responce.data.results);
           })
           .catch(error => {
               console.log(error);
@@ -19,9 +21,13 @@ useEffect (() => {
 
   return (
     <div className="CardContainer">
-     <Card /> 
-     <Card /> 
-     <Card /> 
+     {
+         people.map(people => {
+             return(
+                 <Card />
+             )
+         })
+     }
     </div>
   );
 }
