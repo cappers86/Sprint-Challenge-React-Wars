f9b0521c42f2167727a03cbf45eb3d4a40d6858d
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-11-08 10:34:59 +0000

    Added generic styling to the whole card will no style indiviual tags

--- starwars/src/components/StyledCard.js
+++ starwars/src/components/StyledCard.js
@@ -3,7 +3,18 @@ import styled from 'styled-components';
 
 
 const StyledCard = styled.div`
-   color: green;
+   color: palevioletred;
+   border: 2px solid palevioletred;
+   background: papayawhip;
+   border-radius: 10px;
+   width: 350px;
+   display: flex;
+   flex-direction: column; 
+   align-items: center;
+   justify-content: center;
+   padding: 20px;
+   margin: 10px auto;
+   
 `;
 
 export default StyledCard;
