4b75ba814612f6898d53a5c54314106c6f7aabd0
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-11-08 09:48:45 +0000

    Added information to the card working on styling

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -7,6 +7,13 @@ const Card = (props) => {
   return (
     <div className="Card">
       <h1>{props.people.name}</h1>
+      <h2>Birth year: {props.people.birth_year}</h2>
+      <h2>Gender: {props.people.gender}</h2>
+      <h2>Hair color: {props.people.hair_color}</h2>
+      <h2>Height: {props.people.height}</h2>
+      <h2>Mass: {props.people.mass}</h2>
+      <h2>Skin color: {props.people.skin_color}</h2>
+      
     </div>
   );
 }
