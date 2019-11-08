4c05019b4e3ca8192826659793274b4b502a3b57
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-11-08 08:27:39 +0000

    Checked to see if card container displays

---
+++ starwars/src/components/CardContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const CardContainer = () => {
+
+
+  return (
+    <div className="CardContainer">
+      test
+    </div>
+  );
+}
+
+export default CardContainer ;
